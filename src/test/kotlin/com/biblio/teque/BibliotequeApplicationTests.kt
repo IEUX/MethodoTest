2ba19ad10b3fd83bc5a4271d6bbdc0548a1ff281
IEUX <matheo.plasz@gmail.com>
2025-07-01 12:15:31 +0200

    spring init

---
+++ src/test/kotlin/com/biblio/teque/BibliotequeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.biblio.teque
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BibliotequeApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
