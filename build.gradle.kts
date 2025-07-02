de1a348381f9a8b7bcf6e8d603a17ee35cbc53c5
IEUX <matheo.plasz@gmail.com>
2025-07-02 10:03:45 +0200

    change java distribution in workflow

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,12 +23,31 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter")
 	implementation("org.jetbrains.kotlin:kotlin-reflect")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testImplementation("io.kotest:kotest-runner-junit5:5.9.1")
-	testImplementation("io.kotest:kotest-assertions-core:5.9.1")
-    testImplementation("io.mockk:mockk:1.13.9")
 	testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
 	testImplementation("io.kotest:kotest-property:5.9.1")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+	testIntegrationImplementation("io.mockk:mockk:1.13.8")
+	testIntegrationImplementation("io.kotest:kotest-assertions-core:5.9.1")
+	testIntegrationImplementation("io.kotest:kotest-runner-junit5:5.9.1")
+	testIntegrationImplementation("com.ninja-squad:springmockk:4.0.2")
+	testIntegrationImplementation("io.kotest.extensions:kotest-extensions-spring:1.3.0")
+	testIntegrationImplementation("org.springframework.boot:spring-boot-starter-test") {
+		exclude(module = "mockito-core")
+	}
+}
+
+testing {
+	suites {
+		val testIntegration by registering(JvmTestSuite::class) {
+			sources {
+				kotlin {
+					setSrcDirs(listOf("src/testIntegration/kotlin"))
+				}
+				compileClasspath += sourceSets.main.get().output
+				runtimeClasspath += sourceSets.main.get().output
+			}
+		}
+	}
 }
 
 kotlin {
